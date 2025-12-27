8a090209788191935a803783921debf20042cb5d
imanuel1984 <imanuel1984@outlook.com>
2025-12-27 23:54:18 +0200

    Delete stransport directory

--- stransport/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Profile, TransportRequest, TransportAssignment
-
-admin.site.register(Profile)
-admin.site.register(TransportRequest)
-admin.site.register(TransportAssignment)
